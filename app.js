0403790fba346070175c6bca1663db52666fc8ce
Amisha <amishas171@gmail.com>
2022-08-17 21:12:11 +1200

    Implemented changes to help the responsiveness of the site, however, there is stil much to be done!

--- app.js
+++ app.js
@@ -6,42 +6,46 @@ const highlightMenu = () => {
   const projects = document.querySelector("#projects-page");
   const contact = document.querySelector("#contact-page");
   let scrollPos = window.scrollY;
-  console.log(window.innerWidth);
 
   // now add 'highlight' class to my menu items...
-
   // checking if it's desktop display for highlights.
 
-  if (window.innerWidth > 960 && scrollPos < 600) {
+  if (window.innerWidth > 960 && scrollPos < 500) {
     about.classList.add("highlight");
     skills.classList.remove("highlight");
     contact.classList.remove("highlight");
     projects.classList.remove("highlight");
+    document.getElementById("page-title").classList.remove("disappear");
     return;
-  } else if (window.innerWidth > 960 && scrollPos < 1300) {
+  } else if (window.innerWidth > 960 && scrollPos < 1150) {
     skills.classList.add("highlight");
     about.classList.remove("highlight");
     projects.classList.remove("highlight");
     contact.classList.remove("highlight");
+    document.getElementById("page-title").classList.remove("disappear");
     return;
-  } else if (window.innerWidth > 960 && scrollPos < 1600) {
+  } else if (window.innerWidth > 960 && scrollPos < 1410) {
     projects.classList.add("highlight");
     skills.classList.remove("highlight");
     contact.classList.remove("highlight");
     about.classList.remove("highlight");
+    document.getElementById("page-title").classList.remove("disappear");
     return;
   } else if (window.innerWidth > 960 && scrollPos < 2300) {
     contact.classList.add("highlight");
     projects.classList.remove("highlight");
     about.classList.remove("highlight");
     skills.classList.remove("highlight");
+    document.getElementById("page-title").classList.remove("disappear");
     return;
   }
 
-  if (element && window.innerWidth < 960) {
+  if (window.innerWidth < 960) {
     element.classList.remove("highlight");
+    document.getElementById("page-title").classList.add("disappear");
   }
 };
 
 window.addEventListener("scroll", highlightMenu);
+window.addEventListener("resize", highlightMenu);
 window.addEventListener("click", highlightMenu);
